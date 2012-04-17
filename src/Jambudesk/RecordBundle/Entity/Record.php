1ee04e0e3b35dcff88db47c96586a605f349e790
Dhanushka Samarakoon <email@dhanushka.net>
2012-04-17 09:25:36 -0500

    Added Record/RecordField Entities

---
+++ src/Jambudesk/RecordBundle/Entity/Record.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Jambudesk\RecordBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jambudesk\RecordBundle\Entity\Record
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Record
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+	
+	/**
+	 * @var Form $form
+	 *
+     * @ORM\ManyToOne(targetEntity="Form")
+     * @ORM\JoinColumn(name="formId", referencedColumnName="id")
+     */
+    protected $form;
+    
+    /**
+     * @var datetime $createdOn
+     *
+     * @ORM\Column(name="createdOn", type="datetime")
+     */
+    private $createdOn;
+    
+    /**
+     * @var datetime $lastModifiedOn
+     *
+     * @ORM\Column(name="lastModifiedOn", type="datetime")
+     */
+    private $lastModifiedOn;
+	
+	 /**
+     * @ORM\OneToMany(targetEntity="RecordOption", mappedBy="record")
+     */
+    protected $recordOptions;
+	
+	
+	public function __construct()
+    {
+        $this->recordOptions = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdOn
+     *
+     * @param datetime $createdOn
+     */
+    public function setCreatedOn($createdOn)
+    {
+        $this->createdOn = $createdOn;
+    }
+
+    /**
+     * Get createdOn
+     *
+     * @return datetime 
+     */
+    public function getCreatedOn()
+    {
+        return $this->createdOn;
+    }
+
+    /**
+     * Set lastModifiedOn
+     *
+     * @param datetime $lastModifiedOn
+     */
+    public function setLastModifiedOn($lastModifiedOn)
+    {
+        $this->lastModifiedOn = $lastModifiedOn;
+    }
+
+    /**
+     * Get lastModifiedOn
+     *
+     * @return datetime 
+     */
+    public function getLastModifiedOn()
+    {
+        return $this->lastModifiedOn;
+    }
+
+    /**
+     * Set form
+     *
+     * @param Jambudesk\RecordBundle\Entity\Form $form
+     */
+    public function setForm(\Jambudesk\RecordBundle\Entity\Form $form)
+    {
+        $this->form = $form;
+    }
+
+    /**
+     * Get form
+     *
+     * @return Jambudesk\RecordBundle\Entity\Form 
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+    
+    /**
+     * Add recordOptions
+     *
+     * @param Jambudesk\RecordBundle\Entity\RecordOption $recordOptions
+     */
+    public function addRecordOption(\Jambudesk\RecordBundle\Entity\RecordOption $recordOptions)
+    {
+        $this->recordOptions[] = $recordOptions;
+    }
+
+    /**
+     * Get recordOptions
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getRecordOptions()
+    {
+        return $this->recordOptions;
+    }
+}
---
+++ src/Jambudesk/RecordBundle/Entity/RecordField.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Jambudesk\RecordBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jambudesk\RecordBundle\Entity\RecordField
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class RecordField
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Record $record
+     *
+     * @ORM\ManyToOne(targetEntity="Record", inversedBy="recordOptions")
+     * @ORM\JoinColumn(name="record_id", referencedColumnName="id")
+     */
+    private $record;
+
+    /**
+     * @var Field $field
+     *
+     * @ORM\ManyToOne(targetEntity="Field")
+     * @ORM\JoinColumn(name="fieldId", referencedColumnName="id")
+     */
+    private $field;
+
+    /**
+     * @var FieldOption $fieldOption
+     *
+     * @ORM\ManyToOne(targetEntity="FieldOption")
+     * @ORM\JoinColumn(name="fieldOptionId", referencedColumnName="id")
+     */
+    private $fieldOption;
+
+    /**
+     * @var text $textValue
+     *
+     * @ORM\Column(name="textValue", type="text")
+     */
+    private $textValue;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set textValue
+     *
+     * @param text $textValue
+     */
+    public function setTextValue($textValue)
+    {
+        $this->textValue = $textValue;
+    }
+
+    /**
+     * Get textValue
+     *
+     * @return text 
+     */
+    public function getTextValue()
+    {
+        return $this->textValue;
+    }
+
+    /**
+     * Set record
+     *
+     * @param Jambudesk\RecordBundle\Entity\Record $record
+     */
+    public function setRecord(\Jambudesk\RecordBundle\Entity\Record $record)
+    {
+        $this->record = $record;
+    }
+
+    /**
+     * Get record
+     *
+     * @return Jambudesk\RecordBundle\Entity\Record 
+     */
+    public function getRecord()
+    {
+        return $this->record;
+    }
+
+    /**
+     * Set field
+     *
+     * @param Jambudesk\RecordBundle\Entity\Field $field
+     */
+    public function setField(\Jambudesk\RecordBundle\Entity\Field $field)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * Get field
+     *
+     * @return Jambudesk\RecordBundle\Entity\Field 
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * Set fieldOption
+     *
+     * @param Jambudesk\RecordBundle\Entity\FieldOption $fieldOption
+     */
+    public function setFieldOption(\Jambudesk\RecordBundle\Entity\FieldOption $fieldOption)
+    {
+        $this->fieldOption = $fieldOption;
+    }
+
+    /**
+     * Get fieldOption
+     *
+     * @return Jambudesk\RecordBundle\Entity\FieldOption 
+     */
+    public function getFieldOption()
+    {
+        return $this->fieldOption;
+    }
+}
